2b80c731ecac51ac42733324d6b934d95f8fb70c
cai <cai_scheidler@berkeley.edu>
2026-05-14 13:31:55 -0700

    added to do

--- crb_bot.py
+++ crb_bot.py
@@ -5,6 +5,21 @@ from crb_commands import CRBCommands
 from gemini_commands import GeminiCommands
 from gemini_commands import MarkdownUICommands
 
+# TO DO:
+ # save pages into new commands using a command + markdown for page
+
+ # delete pages using a command (have user confirm in message just sent to user?)
+
+ # save a page into its own page text file in pages on creation
+  # let saving a page overwrite existing pages
+
+ # load all pages from their own text files in pages on start
+
+ # get all pages using a command (give back raw markdown) (just send this to the user and let them delete it after)
+
+ # help page (maybe make all commands self-documenting and then put on this page, make a Cog that takes an arbitrary list of Cogs as input)
+
+ # make announcement template page and then use for pinging meetings using Google Calendar Integration
 
 BOT_TOKEN = "MTUwNDU1NTI1ODcxMjU1NTU0MA.GGPrAs.I_ersnCUT-3YbhLqoRkk2jbXvdBTLMgyhDfzFY"
 
