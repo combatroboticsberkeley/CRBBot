77e6ee2d74583f750d80ab8c0054b3b035764475
cai <cai_scheidler@berkeley.edu>
2026-05-14 19:26:45 -0700

    almost working

--- misc_commands.py
+++ misc_commands.py
@@ -1,4 +1,5 @@
 from nextcord.ext import commands
+import nextcord
 
 import asyncio
 
@@ -15,3 +16,13 @@ class MiscCommands(commands.Cog):
         await ctx.reply(f'Succesfully set Reminder "{text}" for in {mins} mins!')
         await asyncio.sleep(60*mins)
         await ctx.reply(f"REMINDER for {ctx.author.mention}: {text}")
+
+    @commands.command(aliases=["rmdthem"])
+    async def remind_them(self, ctx:commands.Context, user:str, text:str, mins:float):
+        """
+        Reminds the user of "text" after "mins" minutes
+        """
+        userMention = nextcord.utils.get(ctx.guild.members, name=user).mention  # type: ignore
+        await ctx.reply(f'Succesfully set Reminder for {userMention} of "{text}" for in {mins} mins!')
+        await asyncio.sleep(60*mins)
+        await ctx.reply(f"REMINDER for {userMention}: {text}")
